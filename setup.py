08b4d5e3772b32880d4227e321d6be3f483cc646
Keycatowo <owo.nthu@outlook.com>
2023-03-20 23:47:35 +0800

    choco: 更新環境依賴

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rough-set",
+    name="roughset",
     version="0.1.0",
     author="owo",
     author_email="contact@owomail.cc",

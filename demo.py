4188dbe3178bdfb3f28962b33e3de1dd036cb303
Keycatowo <owo.nthu@outlook.com>
2023-03-23 15:33:24 +0800

    feat: 新增可去除重複規則

--- demo.py
+++ demo.py
@@ -40,12 +40,17 @@ if st.session_state.submit:
     
 
     
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4 = st.columns(4)
     with col1: support_th = st.number_input("Support Threshold", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
     with col2: confidence_th = st.number_input("Confidence Threshold", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
     with col3: lift_th = st.number_input("Lift Threshold", min_value=0.0, value=0.0, step=0.01)
+    with col4: deduplicate = st.checkbox("Deduplicate", value=True)
 
     rules_filtered = rules_with_metrics[(rules_with_metrics["support"] >= support_th) & (rules_with_metrics["confidence"] >= confidence_th) & (rules_with_metrics["lift"] >= lift_th)]
+    if deduplicate:
+        rules_filtered = rules_filtered.drop(columns=[name_col])
+        rules_filtered = rules_filtered.drop_duplicates()
     st.dataframe(rules_filtered, use_container_width=True)
+    st.write("Number of rules:", rules_filtered.shape[0])
     st.download_button("Download", convert_df(rules_filtered), file_name="rules.csv", mime="text/csv")
     

cc6bccd08329dd400f2b7019a06f51f2f9b58b9d
Keycatowo <owo.nthu@outlook.com>
2023-07-18 09:56:12 +0800

    feat: 新增特徵欄位計算Beta版本 + 比較兩種選擇欄位是否獨立 + 計算近似空間

---
+++ pages/特徵欄位.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+
+st.header("計算約略集合欄位")
+st.warning("此功能目前為Beta版本")
+
+st.subheader("Upload a CSV file")
+file = st.file_uploader("Upload a CSV file", type="csv")
+
+if file is not None:
+    df = pd.read_csv(file)
+    st.write(df)
+
+    with st.expander("比較欄位是否獨立"):    
+        col1, col2 = st.columns(2)
+        
+        name_col = st.selectbox("物件名稱欄位", list(df.columns), index=0)
+        decision_col = st.selectbox("決策欄位", list(df.columns), index=len(df.columns)-1)
+        with col1:
+            select_col1 = st.multiselect("比較欄位1", list(df.columns), default=list(df.columns[1:-1]))
+            grouped1 = df.groupby(select_col1)[name_col].apply(list)
+            D1 = list(grouped1.values)
+            st.write(D1)
+        with col2:
+            select_col2 = st.multiselect("比較欄位2", list(df.columns), default=list(df.columns[1:-2]))
+            grouped2 = df.groupby(select_col2)[name_col].apply(list)
+            D2 = list(grouped2.values)
+            st.write(D2)
+        st.write("是否相同:", D1 == D2)    
+    
+    with st.expander("計算近似空間"):
+    
+        unique_decision = list(df[decision_col].unique())
+        X_value = st.selectbox("集合X的值", unique_decision, index=0)
+        X = df.groupby(decision_col)[name_col].apply(list)[X_value]
+        st.write(f"集合X={X_value}: {{{X}}}")
+        select_col = st.multiselect("屬性集合D", list(df.columns), default=list(df.columns[1:-1]))
+        grouped = df.groupby(select_col)[name_col].apply(list)
+        D = list(grouped.values)
+        
+        col3, col4 = st.columns(2)
+        with col3:
+            st.write("集合X的下近似集合")
+            lower_set = set()
+            for x in X:
+                if [x] in D:
+                    lower_set.add(x)
+            st.write(lower_set)
+        with col4:
+            st.write("集合X的上近似集合")
+            upper_set = set()
+            for x in X:
+                for d in D:
+                    if x in d:
+                        upper_set.update(d)
+            st.write(upper_set)
+        st.write("界限集合")
+        st.write(upper_set - lower_set)
+            
+            
+    
+    

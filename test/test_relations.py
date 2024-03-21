5119bf77ff44179b5d288e7d4622d2ce94412229
Keycatowo <owo.nthu@outlook.com>
2024-03-22 02:22:02 +0800

    test: 測試get_upper_approximation

--- test/test_relations.py
+++ test/test_relations.py
@@ -1,4 +1,4 @@
-from roughset.relations import get_equivalence_object, get_lower_approximation
+from roughset.relations import get_equivalence_object, get_lower_approximation, get_upper_approximation
 import pandas as pd
 
 def test_get_equivalence_object_example1():
@@ -76,3 +76,36 @@ def test_get_lower_approximation_example3():
     df = pd.read_csv("Blood_disease.csv")
     
     assert get_lower_approximation(df, "Sample", ["Blood type"], "Disease", 1) == set()
+    
+    
+def test_get_upper_approximation_example1():
+    """
+        以範例1測試 get_upper_approximation
+        參考值來自於 《大數據分析與資料挖礦》 p.122
+    """
+    
+    df = pd.read_csv('example.csv')
+    
+    assert get_upper_approximation(df, "No", ["天氣", "事故情形"], "損壞部位", 1) == {1, 3, 4, 5}
+    
+def test_get_upper_approximation_example2():
+    """
+        以範例2測試 get_upper_approximation
+        參考值來自於 《大數據分析與資料挖礦》 p.135
+    """
+    
+    df = pd.read_csv('Mohapatra.csv')
+    
+    assert get_upper_approximation(df, "Company", ["Mkt(a1)", "Dist(a3)"], "Sales(D)", "H") == {'C10', 'C15', 'C19', 'C2', 'C7'}
+    assert get_upper_approximation(df, "Company", ["Mkt(a1)", "Dist(a3)"], "Sales(D)", "A") == {'C15', 'C19', 'C2', 'C7'}
+    assert get_upper_approximation(df, "Company", ["Mkt(a1)", "Dist(a3)"], "Sales(D)", "L") == {'C1', 'C11', 'C12', 'C13', 'C14', 'C16', 'C17', 'C18', 'C20', 'C21', 'C22', 'C23', 'C3', 'C4', 'C5', 'C6', 'C8', 'C9'}
+
+def test_get_upper_approximation_example3():
+    """
+        以範例3測試 get_upper_approximation
+        
+    """
+    
+    df = pd.read_csv("Blood_disease.csv")
+    
+    assert get_upper_approximation(df, "Sample", ["Blood type"], "Disease", 1) == {1, 2, 3, 4, 5, 7}

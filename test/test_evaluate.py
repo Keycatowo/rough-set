a052e780244707754a0d10ea440f0b216ff4be50
Keycatowo <owo.nthu@outlook.com>
2024-03-22 11:46:11 +0800

    test: 測試 evaluate_metrics

--- test/test_evaluate.py
+++ test/test_evaluate.py
@@ -1,5 +1,5 @@
 #%%
-from roughset.evaluate import calculate_rules_ratio
+from roughset.evaluate import calculate_rules_ratio, evaluate_metrics
 import pandas as pd
 
 def test_calculate_rules_ratio_example1():
@@ -120,3 +120,59 @@ def test_calculate_rules_ratio_example1():
         df
     ) == 0.2
     
+    
+def test_evaluate_metrics_example1():
+    
+    df = pd.read_csv('example.csv')
+    name_col="No"
+    feature_col=['天氣', '事故情形', '事故原因']
+    decision_col='損壞部位'
+    
+    assert evaluate_metrics(
+        {
+            "天氣": None,
+            "事故情形": 0,
+            "事故原因": None,
+            "損壞部位": 0
+        },
+        df_data=df,
+        name_col=name_col,
+        feature_col=feature_col,
+        decision_col=decision_col   
+    ) == {"support": 0.2, "confidence": 1, "lift": 2.5}
+    assert evaluate_metrics(
+        {
+            "天氣": 1,
+            "事故情形": 0,
+            "事故原因": None,
+            "損壞部位": 0
+        },
+        df_data=df,
+        name_col=name_col,
+        feature_col=feature_col,
+        decision_col=decision_col   
+    ) == {"support": 0.2, "confidence": 1, "lift": 2.5}
+    assert evaluate_metrics(
+        {
+            "天氣": None,
+            "事故情形": 0,
+            "事故原因": 0,
+            "損壞部位": 0
+        },
+        df_data=df,
+        name_col=name_col,
+        feature_col=feature_col,
+        decision_col=decision_col   
+    ) == {"support": 0.2, "confidence": 1, "lift": 2.5}
+    assert evaluate_metrics(
+        {
+            "天氣": None,
+            "事故情形": 1,
+            "事故原因": 0,
+            "損壞部位": 1
+        },
+        df_data=df,
+        name_col=name_col,
+        feature_col=feature_col,
+        decision_col=decision_col   
+    ) == {"support": 0.2, "confidence": 1, "lift": 5/3}

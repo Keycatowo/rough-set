49a380b3aefc7580a29e1a42d14a93ab70de3792
Keycatowo <owo.nthu@outlook.com>
2024-03-25 14:13:59 +0800

    test: 修正浮點數assert錯誤
    使用numpy.testing.assert_allclose取代assert
    不使用直接比較dict，而是取出values比較

--- test/test_evaluate.py
+++ test/test_evaluate.py
@@ -1,6 +1,7 @@
 #%%
 from roughset.evaluate import calculate_rules_ratio, evaluate_metrics
 import pandas as pd
+from numpy.testing import assert_allclose
 
 def test_calculate_rules_ratio_example1():
     
@@ -8,117 +9,117 @@ def test_calculate_rules_ratio_example1():
 
     
     # 單一條件
-    assert calculate_rules_ratio({
+    assert_allclose(calculate_rules_ratio({
         "天氣": 0
         },
         df
-    ) == 0.4
-    assert calculate_rules_ratio({
+    ), 0.4)
+    assert_allclose(calculate_rules_ratio({
         "天氣": 0,
         "事故情形": None
         },
         df
-    ) == 0.4
-    assert calculate_rules_ratio({
+    ), 0.4)
+    assert_allclose(calculate_rules_ratio({
         "天氣": 0,
         "事故情形": None,
         "事故原因": None
         },
         df
-    ) == 0.4
-    assert calculate_rules_ratio({
+    ), 0.4)
+    assert_allclose(calculate_rules_ratio({
         "天氣": 0,
         "事故情形": None,
         "事故原因": None,
         "損壞部位": None
         },
         df
-    ) == 0.4
+    ), 0.4)
     
-    assert calculate_rules_ratio({
+    assert_allclose(calculate_rules_ratio({
         "天氣": 1
         },
         df
-    ) == 0.6
-    assert calculate_rules_ratio({
+    ), 0.6)
+    assert_allclose(calculate_rules_ratio({
         "事故情形": 0
         },
         df
-    ) == 0.2
-    assert calculate_rules_ratio({
+    ), 0.2)
+    assert_allclose(calculate_rules_ratio({
         "事故情形": 1
         },
         df
-    ) == 0.6
-    assert calculate_rules_ratio({
+    ), 0.6)
+    assert_allclose(calculate_rules_ratio({
         "事故情形": 2
         },
         df
-    ) == 0.2
-    assert calculate_rules_ratio({
+    ), 0.2)
+    assert_allclose(calculate_rules_ratio({
         "事故原因": 0
         },
         df
-    ) == 0.4
-    assert calculate_rules_ratio({
+    ), 0.4)
+    assert_allclose(calculate_rules_ratio({
         "事故原因": 1
         },
         df
-    ) == 0.6
-    assert calculate_rules_ratio({
+    ), 0.6)
+    assert_allclose(calculate_rules_ratio({
         "損壞部位": 0
         },
         df
-    ) == 0.4
-    assert calculate_rules_ratio({
+    ), 0.4)
+    assert_allclose(calculate_rules_ratio({
         "損壞部位": 1
         },
         df
-    ) == 0.6
+    ), 0.6)
     
     # 多條件
-    assert calculate_rules_ratio(
+    assert_allclose(calculate_rules_ratio(
         {
             "天氣": 0,
             "事故情形": 0
         },
         df
-    ) == 0
-    assert calculate_rules_ratio(
+    ), 0)
+    assert_allclose(calculate_rules_ratio(
         {
             "天氣": 0,
             "事故情形": 1
         },
         df
-    ) == 0.4
-    assert calculate_rules_ratio(
+    ), 0.4)
+    assert_allclose(calculate_rules_ratio(
         {
             "天氣": 1,
             "事故情形": 0
         },
         df
-    ) == 0.2
-    assert calculate_rules_ratio(
+    ), 0.2)
+    assert_allclose(calculate_rules_ratio(
         {
             "天氣": 1,
             "事故情形": 1
         },
         df
-    ) == 0.2
-    assert calculate_rules_ratio(
+    ), 0.2)
+    assert_allclose(calculate_rules_ratio(
         {
             "天氣": 1,
             "事故情形": 2
         },
         df
-    ) == 0.2
-    assert calculate_rules_ratio(
+    ), 0.2)
+    assert_allclose(calculate_rules_ratio(
         {
             "事故情形": 0,
             "事故原因": 0
         },
         df
-    ) == 0.2
+    ), 0.2)
     
     
 def test_evaluate_metrics_example1():
@@ -128,7 +129,7 @@ def test_evaluate_metrics_example1():
     feature_col=['天氣', '事故情形', '事故原因']
     decision_col='損壞部位'
     
-    assert evaluate_metrics(
+    metrics = evaluate_metrics(
         {
             "天氣": None,
             "事故情形": 0,
@@ -139,8 +140,11 @@ def test_evaluate_metrics_example1():
         name_col=name_col,
         feature_col=feature_col,
         decision_col=decision_col   
-    ) == {"support": 0.2, "confidence": 1, "lift": 2.5}
-    assert evaluate_metrics(
+    )
+    assert_allclose(metrics["support"], 0.2)
+    assert_allclose(metrics["confidence"], 1.0)
+    assert_allclose(metrics["lift"], 2.5)
+    metrics = evaluate_metrics(
         {
             "天氣": 1,
             "事故情形": 0,
@@ -151,8 +155,12 @@ def test_evaluate_metrics_example1():
         name_col=name_col,
         feature_col=feature_col,
         decision_col=decision_col   
-    ) == {"support": 0.2, "confidence": 1, "lift": 2.5}
-    assert evaluate_metrics(
+    )
+    assert_allclose(metrics["support"], 0.2)
+    assert_allclose(metrics["confidence"], 1.0)
+    assert_allclose(metrics["lift"], 2.5)
+
+    metrics = evaluate_metrics(
         {
             "天氣": None,
             "事故情形": 0,
@@ -163,8 +171,12 @@ def test_evaluate_metrics_example1():
         name_col=name_col,
         feature_col=feature_col,
         decision_col=decision_col   
-    ) == {"support": 0.2, "confidence": 1, "lift": 2.5}
-    assert evaluate_metrics(
+    )
+    assert_allclose(metrics["support"], 0.2)
+    assert_allclose(metrics["confidence"], 1.0)
+    assert_allclose(metrics["lift"], 2.5)
+
+    metrics = evaluate_metrics(
         {
             "天氣": None,
             "事故情形": 1,
@@ -175,4 +187,7 @@ def test_evaluate_metrics_example1():
         name_col=name_col,
         feature_col=feature_col,
         decision_col=decision_col   
-    ) == {"support": 0.2, "confidence": 1, "lift": 5/3}
+    )
+    assert_allclose(metrics["support"], 0.2)
+    assert_allclose(metrics["confidence"], 1.0)
+    assert_allclose(metrics["lift"], 5/3)
